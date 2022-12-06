4575a337241db9a13f526d6b6846e8f2173ddc95
Jefke <36000897+Jefkeh@users.noreply.github.com>
2022-12-05 19:05:54 -0600

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,11 @@ import express, { Request, Response } from "express";
 
 const app = express();
 const port = process.env.PORT || 3000;
-const path = require('path');
 
-
-const fs = require('fs');
-fs.readFileSync(`${__dirname}\\index.html`);
+app.use(express.static('public'))
 
 app.get("/", async (req: Request, res: Response) => {
-  //res.send("Typescript ExpressJS: Hello world, Jefke trying out things");
-  res.sendFile(path.join(__dirname, './index.html'));
+  res.send("Typescript ExpressJS: Hello world, Jefke trying out things");
 });
 
 app.listen(port, () => {
